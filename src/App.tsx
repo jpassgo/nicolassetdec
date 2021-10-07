f592e4de12e01b67c04c51bae886bf1030c9f799
jpassgo <jeffpascoe9@gmail.com>
2021-10-06 20:30:58 -0500

    Updating app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import createMuiTheme, { Theme } from '@material-ui/core/styles/createMuiTheme';
+import { MuiThemeProvider, createMuiTheme, Theme } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
-import { createBrowserHistory } from 'history';
 import { Route, Router } from 'react-router';
 import Grid from '@material-ui/core/Grid';
-import NavigationDrawer from './components/NavigationDrawer';
 import { State } from './config/store';
-import LoginPage from './components/LoginPage';
 import routes from './constants/routes';
-import CreateAccountPage from './components/CreateAccountPage';
 import Title from './components/Title';
-import ReportMealsPage from './components/ReportMealsPage';
+import Links from './components/Links';
 
 export function getLightTheme(): Theme {
   return createMuiTheme();
@@ -29,24 +24,12 @@ const App = (): JSX.Element => (
         alignItems="flex-start"
         justify="center"
       >
-        <Grid item xs={5} style={{ marginLeft: '2.5%' }}>
-          <NavigationDrawer />
-        </Grid>
         <Grid item xs>
           <Title />
         </Grid>
       </Grid>
 
-      <Route path={routes.HOME_PAGE} />
-      <Route path={routes.LOGIN_PAGE}>
-        <LoginPage />
-      </Route>
-      <Route path={routes.CREATE_ACCOUNT_PAGE}>
-        <CreateAccountPage />
-      </Route>
-      <Route path={routes.REPORT_MEALS_PAGE}>
-        <ReportMealsPage />
-      </Route>
+      <Links />
     </Router>
   </MuiThemeProvider>
 );
